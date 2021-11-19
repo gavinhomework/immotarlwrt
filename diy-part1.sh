1be61cf2604ea515f588299f638bfa741f2c7389
gavinhomework <92965736+gavinhomework@users.noreply.github.com>
2021-11-19 12:18:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,8 @@
 
 # Add a feed source
 
-echo 'src-git node https://github.com/nxhack/openwrt-node-packages' >>feeds.conf.default
+echo 'src-git node https://github.com/nxhack/openwrt-node-packages.git;openwrt-21.02
+' >>feeds.conf.default
 
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
